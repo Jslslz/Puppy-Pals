72eea4b991b071c8f0b1403b0ea613738f172512
Joseluis Loza <luis@Joseluiss-MacBook-Pro.local>
2023-07-23 13:31:50 -0700

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
   return (
     <div>
-
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>
+      })}
     </div>
   )
 }
